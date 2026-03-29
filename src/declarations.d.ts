57620fe520db14109021d8cc701cc03a04c1e1ba
Jakub <50637007+Depok1@users.noreply.github.com>
2026-03-30 00:05:14 +0200

    fix(declarations.d.ts): same configuration that works in dev and build

--- src/declarations.d.ts
+++ src/declarations.d.ts
@@ -2,3 +2,9 @@ declare module "*.module.scss" {
   const classes: Record<string, string>;
   export default classes;
 }
+
+declare module "*.svg?react" {
+  import type { FC, SVGProps } from "react";
+  const content: FC<SVGProps<SVGSVGElement>>;
+  export default content;
+}
